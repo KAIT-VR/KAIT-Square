a572700c3b1bc3a10cce07094bcb5992971ac3d8
Tsuntsuku <156342916+tsuntsuku1120@users.noreply.github.com>
2026-01-12 21:43:11 +0900

    床マテリアル修正およびスピーカーモデル仮置き

---
+++ Assets/KinelVideoPlayerPlugin/Udon/KinelVideoPlayerResyncer.cs
@@ -0,0 +1,33 @@
+﻿using Kinel.VideoPlayer.Udon;
+using Kinel.VideoPlayer.Udon.Module;
+using UnityEngine;
+
+namespace KinelVideoPlayerPlugin.Udon
+{
+    public class KinelVideoPlayerResyncer : KinelModule
+    {
+        [SerializeField] private KinelVideoPlayer[] videoPlayers;
+
+        public void Start()
+        {
+        }
+
+        public void ResyncAll()
+        {
+            Debug.Log($"{DEBUG_PREFIX} ResyncAll");
+            for (int i = 0; i < videoPlayers.Length; i++)
+            {
+                videoPlayers[i].Sync();
+            }
+        }
+
+        public void ReloadAll()
+        {
+            Debug.Log($"{DEBUG_PREFIX} ReloadAll");
+            for (int i = 0; i < videoPlayers.Length; i++)
+            {
+                videoPlayers[i].Reload();
+            }
+        }
+    }
+}
